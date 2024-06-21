4dd8bac96f7f2904302950750a3621e678580f3d
numLinka <numlinka@163.com>
2024-06-21 23:21:41 +0800

    Correct setup package dir

--- setup.py
+++ setup.py
@@ -8,14 +8,15 @@ from setuptools import setup
 
 setup(
     name = "i18nco",
-    version = "1.2.1",
+    version = "1.2.2",
     description = "This is a simple and easy to use Python i18n library.",
     long_description = open("README_PyPI.md", "r", encoding="utf-8").read(),
     long_description_content_type = "text/markdown",
     author = "numlinka",
     author_email = "numlinka@163.com",
     url = "https://github.com/numlinka/pyi18nco",
-    packages = ["src/i18nco"],
+    package_dir={"": "src"},
+    packages = ["i18nco"],
     classifiers=[
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Programming Language :: Python :: 3",
